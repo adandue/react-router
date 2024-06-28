dde91bd693c87541e3cf8d129ca0083e96532870
adan_due <ing.adanduenas@gmail.com>
2024-06-28 14:47:15 -0600

    Roles y permisos

--- react-router/src/auth.jsx
+++ react-router/src/auth.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
+
+const adminList = ['adan_due', 'vero', 'aurora', 'yuki']
+
 const AuthContext = React.createContext()
 
 function AuthProvider({ children }){
@@ -8,7 +11,8 @@ function AuthProvider({ children }){
     const [user, setUser] = React.useState(null)
 
     const login = ({ username }) => {
-        setUser({ username })
+        const isAdmin = adminList.find(admin => admin === username)
+        setUser({ username, isAdmin })
         navigate('/profile')
     }
 
