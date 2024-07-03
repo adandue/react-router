ee4fb40bed745cea0298aff4e6a6073d6a1f5c99
adan_due <ing.adanduenas@gmail.com>
2024-07-03 16:20:13 -0600

    Implementando los cambios en el blogPage para que se puedan borrar los blogposts

--- react-router/src/BlogPage.jsx
+++ react-router/src/BlogPage.jsx
@@ -1,35 +1,33 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import { useData } from "./blogdata";
-import { CreatePost } from "./CreatePost";
+import React from "react"
+import { Link, Outlet } from "react-router-dom"
+import { blogdata } from "./blogdata"
+import { useAuth } from './auth'
 
-function BlogPage() {
-    const [createPost, setCreatePost ] = React.useState(false);
 
-    const blogdata = useData();
-    return(
-        <React.Fragment>
-            <h2>BlogPage</h2>
-            <ul>
-                {console.log(blogdata)}
-                {blogdata.data.map(post => (<BlogLink  key={post.slug} post={post}/>))}
-            </ul>
-            <Outlet />
-            {!createPost && <button onClick={()=> setCreatePost(true)}>Nuevo Post</button>}
-            {createPost && <CreatePost setCreatePost={setCreatePost} />}  
-        </React.Fragment>
-    );
-};
+const BlogPage = () => {
+    const { data } = useAuth()
 
-function BlogLink({post}) {
-    return(
-        <li>
-            <Link 
-            to={/blog/`${post.slug}`}>
-                {post.title}
-            </Link>
-        </li>
-    );
+    return (
+        <>
+        <h1>Blog</h1>
+
+        <Outlet />
+
+        <ul>
+            {data.map(post => (
+                <BlogLink post={post} key={post.slug} />
+            ))}
+        </ul>
+        </>
+    )
 }
 
-export { BlogPage };
+const BlogLink = ({ post }) => {
+    return (
+        <li>
+            <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+        </li>
+    )
+}
+
+export { BlogPage }
