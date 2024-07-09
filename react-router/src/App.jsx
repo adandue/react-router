cbb87970a4b9fe621e0ec30f794442f9e7ed0f0a
adan_due <ing.adanduenas@gmail.com>
2024-07-09 12:45:29 -0600

    Se actualiza el archivo App

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,57 +1,72 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import { Menu } from './Menu'
-import { AuthProvider, AuthRoute } from './auth'
-import { HomePage } from './HomePage'
-import { BlogPage } from './BlogPage'
-import { LoginPage } from './LoginPage'
-import { LogoutPage } from './LogoutPage'
-import { ProfilePage } from './ProfilePage'
-import { BlogPost } from './BlogPost'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Menu } from './components/Menu';
+import { HomePage } from './pages/HomePage';
+import { BlogPage } from './pages/BlogPage';
+import { ProfilePage } from './pages/ProfilePage';
+import { BlogPost } from './components/BlogPost';
+import { CreateBlog } from './components/CreateBlog';
+import { LoginPage } from './pages/LoginPage';
+import { LogoutPage } from './pages/LogoutPage';
+import { AuthProvider, AuthRoute } from 'services/auth';
+import { BlogProvider, BlogRoute } from 'services/blog';
+import { UsersDBProvider } from 'services/usersDB';
 
 function App() {
-
   return (
-    <>
-      
-      <HashRouter>
+    <HashRouter>
+      <UsersDBProvider>
         <AuthProvider>
           <Menu />
 
-          <Routes>
-            <Route path="/" element={<HomePage />} />
+          <BlogProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
 
-            <Route path="/blog" element={<BlogPage />}>
-              <Route path=":slug" element={<BlogPost />} />
-            </Route>
+              <Route path="/blog" element={<BlogPage />}>
+                <Route
+                  path=":slug"
+                  element={
+                    <BlogRoute>
+                      <BlogPost />
+                    </BlogRoute>
+                  }
+                />
+                <Route path="create" element={<CreateBlog />} />
+              </Route>
 
+              <Route path="/login" element={<LoginPage />} />
+              <Route
+                path="/logout"
+                element={
+                  <AuthRoute>
+                    <LogoutPage />
+                  </AuthRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <AuthRoute>
+                    <ProfilePage />
+                  </AuthRoute>
+                }
+              />
+              <Route
+                path="/profile/:slug"
+                element={
+                  <AuthRoute>
+                    <ProfilePage />
+                  </AuthRoute>
+                }
+              />
 
-            <Route path="/login" element={<LoginPage />} />
-            <Route 
-              path="/logout" 
-              element={
-                <AuthRoute>
-                  <LogoutPage />
-                </AuthRoute>
-              } 
-            />
-            <Route 
-              path="/profile" 
-              element={
-                <AuthRoute>
-                  <ProfilePage />
-                </AuthRoute>
-              } 
-            />
-
-            <Route path="*" element={<p>Not Found</p>} />
-          </Routes>
-
-          <footer></footer>
-
+              <Route path="*" element={<p>Not found</p>} />
+            </Routes>
+          </BlogProvider>
         </AuthProvider>
-      </HashRouter>
-    </>
-  )
+      </UsersDBProvider>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
