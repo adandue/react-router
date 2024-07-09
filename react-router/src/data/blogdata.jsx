31065e7f2122b55beb78f0f8f3dbf7bc257bac24
adan_due <ing.adanduenas@gmail.com>
2024-07-09 12:31:21 -0600

    Se crea la carpeta data con la información de los blogPost, los roles y los usuarios

---
+++ react-router/src/data/blogdata.jsx
@@ -0,0 +1,30 @@
+const blogdata = [
+    {
+        title: '¿Qué es React?',
+        slug: 'que-es-react',
+        content: 'React es el mejor framework de JS.',
+        author: 'luna',
+        comments: [
+        {
+            content: 'Buenos cursos, juandc!\nPor cierto, cada vez hablas más como Freddy xD',
+            author: 'tigre'
+        }
+        ],
+    },
+    {
+        title: '¿Qué es Vue?',
+        slug: 'que-es-vue',
+        content: 'Vue es el mejor framework de JS.',
+        author: 'adan_due',
+        comments: [],
+    },
+    {
+        title: '¿Qué es Angular?',
+        slug: 'que-es-angular',
+        content: 'Angular es el mejor framework de JS.',
+        author: 'tigre',
+        comments: [],
+    },
+    ];
+
+    export { blogdata };
---
+++ react-router/src/data/users.jsx
@@ -0,0 +1,40 @@
+import { ADMIN, USER } from "./roles";
+
+export const users = [
+    {
+    username: 'adan_due',
+    phonenumber: '333',
+    description: 'I\'m Irisval (0,0)/',
+    roles: [ADMIN]
+    },
+    {
+    username: 'vero_montero',
+    phonenumber: '333',
+    description: 'I\'m RetaxMaster (0,0)/',
+    roles: [ADMIN]
+    },
+    {
+    username: 'aurora',
+    phonenumber: '333',
+    description: 'I\'m freddier (0,0)/',
+    roles: [ADMIN]
+    },
+    {
+    username: 'yuki',
+    phonenumber: '333',
+    description: 'I\'m alex (0,0)/',
+    roles: [ADMIN]
+    },
+    {
+    username: 'luna',
+    phonenumber: '333',
+    description: 'I\'m juandc (0,0)/',
+    roles: [USER]
+    },
+    {
+    username: 'tigre',
+    phonenumber: '333',
+    description: 'I\'m nameless (0,0)/',
+    roles: [USER]
+    },
+];
